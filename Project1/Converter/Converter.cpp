df9ec0dd2f5f8c4120595c62265828eac1190f85
wentan <wentan1995@gmail.com>
2019-02-01 19:21:27 -0500

    right version

---
+++ Project1/Converter/Converter.cpp
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////////////////////////
+// Executive.cpp								                   //
+// ver 1.0                                                         //
+// Wentan Bai, CSE687 - Object Oriented Design, Spring 2019        //
+/////////////////////////////////////////////////////////////////////
+#ifdef TEST_CONVERTER
+
+#include "Converter.h"
+
+int main() {
+	// using Converter.cpp and Converter.h to test
+	std::vector<std::string> codePath = {
+		"../Converter/Converter.cpp",
+		"../Converter/Converter.h",
+	};
+	std::string savePath = "../Converter";
+
+	ConverterHtml con(codePath, savePath);
+	con.generateHtml();
+
+	std::vector<std::string>savedPath = con.getHtmlPaths();
+	for (std::string p : savedPath) {
+		std::cout << p << std::endl;
+	}
+
+	return 0;
+}
+#endif
